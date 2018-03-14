9316b68168eaf371f520a0cee087d34a59b5f1c6
Hung-Wei Tseng <htseng3@ncsu.edu>
2018-03-14 16:45:05 -0400

    Initial commit

---
+++ fpga_sort.cpp
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mysort.h"
+#include "CL/opencl.h"
+#include "AOCL_Utils.h"
+
+using namespace aocl_utils;
+
+// OpenCL runtime configuration
+cl_platform_id platform = NULL;
+unsigned num_devices = 0;
+scoped_array<cl_device_id> device; // num_devices elements
+cl_context context = NULL;
+scoped_array<cl_command_queue> queue; // num_devices elements
+cl_program program = NULL;
+scoped_array<cl_kernel> kernel; // num_devices elements
+
+bool init_opencl();
+
+int fpga_sort(int num_of_elements, float *data)
+{
+    init_opencl();
+    return 0;
+}
+
+// Initializes the OpenCL objects.
+bool init_opencl() {
+  cl_int status;
+
+  printf("Initializing OpenCL\n");
+
+  if(!setCwdToExeDir()) {
+    return false;
+  }
+
+  // Get the OpenCL platform.
+  platform = findPlatform("Altera");
+  if(platform == NULL) {
+    printf("ERROR: Unable to find Altera OpenCL platform.\n");
+    return false;
+  }
+
+  // Query the available OpenCL device.
+  device.reset(getDevices(platform, CL_DEVICE_TYPE_ALL, &num_devices));
+  printf("Platform: %s\n", getPlatformName(platform).c_str());
+  printf("Using %d device(s)\n", num_devices);
+  for(unsigned i = 0; i < num_devices; ++i) {
+    printf("  %s\n", getDeviceName(device[i]).c_str());
+  }
+
+  // Create the context.
+  context = clCreateContext(NULL, num_devices, device, NULL, NULL, &status);
+  checkError(status, "Failed to create context");
+
+  // Create the program for all device. Use the first device as the
+  // representative device (assuming all device are of the same type).
+  std::string binary_file = getBoardBinaryFile("fpgasort", device[0]);
+  printf("Using AOCX: %s\n", binary_file.c_str());
+  program = createProgramFromBinary(context, binary_file.c_str(), device, num_devices);
+
+  // Build the program that was just created.
+  status = clBuildProgram(program, 0, NULL, "", NULL, NULL);
+  checkError(status, "Failed to build program");
+
+  // Create per-device objects.
+  queue.reset(num_devices);
+  kernel.reset(num_devices);
+  for(unsigned i = 0; i < num_devices; ++i) {
+    // Command queue.
+    queue[i] = clCreateCommandQueue(context, device[i], CL_QUEUE_PROFILING_ENABLE, &status);
+    checkError(status, "Failed to create command queue");
+
+    // Kernel.
+    const char *kernel_name = "fpgasort";
+    kernel[i] = clCreateKernel(program, kernel_name, &status);
+    checkError(status, "Failed to create kernel");
+
+  }
+
+  return true;
+}
+
+void cleanup() {
+  for(unsigned i = 0; i < num_devices; ++i) {
+    if(kernel && kernel[i]) {
+      clReleaseKernel(kernel[i]);
+    }
+    if(queue && queue[i]) {
+      clReleaseCommandQueue(queue[i]);
+    }
+  }
+
+  if(program) {
+    clReleaseProgram(program);
+  }
+  if(context) {
+    clReleaseContext(context);
+  }
+}
+
