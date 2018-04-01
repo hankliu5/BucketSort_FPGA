4314ca77eb18653ca4df22a2b01ca630a14a158b
hankliu5 <yul560@eng.ucsd.edu>
2018-04-01 15:00:32 -0400

    compilable version.

--- fpga_sort.cpp
+++ fpga_sort.cpp
@@ -1,7 +1,5 @@
-#include <algorithm>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "mysort.h"
 #include <fcntl.h>
 #include <string.h>
@@ -19,23 +17,22 @@
 #include "AOCL_Utils.h"
 using namespace aocl_utils;
 #endif
+
+cl_platform_id platform = NULL;
+cl_context context = NULL;
+cl_program program = NULL;
+
 #ifdef APPLE
 // OpenCL runtime configuration
-cl_platform_id platform = NULL;
 unsigned num_devices = 1;
 cl_device_id device; // num_devices elements
-cl_context context = NULL;
 cl_command_queue queue; // num_devices elements
-cl_program program = NULL;
 cl_kernel kernel; // num_devices elements
 #else
 // OpenCL runtime configuration
-cl_platform_id platform = NULL;
 unsigned num_devices = 0;
 scoped_array<cl_device_id> device; // num_devices elements
-cl_context context = NULL;
 scoped_array<cl_command_queue> queue; // num_devices elements
-cl_program program = NULL;
 scoped_array<cl_kernel> kernel; // num_devices elements
 #endif
 
@@ -51,16 +48,7 @@ void _checkError(int line,
 #define checkError(status, ...) _checkError(__LINE__, __FILE__, status, __VA_ARGS__)
 #endif
 
-// problem data.
-scoped_array< scoped_aligned_ptr<float> > input;
-scoped_array< scoped_aligned_ptr<float> > output;
-
-const int NUM_BUCKETS = 10;
-
-bool init_opencl(int num_of_elements);
-void init_problem(float *data);
-void cl_run(int num_of_elements, float *data);
-void cleanup();
+bool init_opencl();
 
 int fpga_sort(int num_of_elements, float *data)
 {
@@ -124,12 +112,6 @@ bool init_opencl() {
   //Create per-device objects.
   queue.reset(num_devices);
   kernel.reset(num_devices);
-  n_per_device.reset(num_devices);
-  input_buf.reset(num_devices);
-  bucket_buf.reset(num_devices);
-  step_buf.reset(num_devices);
-  output_buf.reset(num_devices);
-
   for(unsigned i = 0; i < num_devices; ++i) {
     // Command queue.
     queue[i] = clCreateCommandQueue(context, device[i], CL_QUEUE_PROFILING_ENABLE, &status);
@@ -140,30 +122,6 @@ bool init_opencl() {
     kernel[i] = clCreateKernel(program, kernel_name, &status);
     checkError(status, "Failed to create kernel");
 
-    // Determine the number of elements processed by this device.
-    n_per_device[i] = num_of_elements / num_devices;
-
-    // Spread out the remainder of the elements over the first
-    // N % num_devices.
-    if (i < (num_of_elements % num_devices)) {
-        n_per_device[i]++;
-    }
-
-    input_buf[i] = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            n_per_device[i] * sizeof(float), NULL, &status);
-    checkError(status, "Failed to create buffer for input");
-
-    bucket_buf[i] = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            NUM_BUCKETS * sizeof(int), NULL, &status);
-    checkError(status, "Failed to create buffer for input");
-
-    step_buf[i] = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            1 * sizeof(int), NULL, &status);
-    checkError(status, "Failed to create buffer for input");
-
-    output_buf[i] = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-            n_per_device[i] * sizeof(float), NULL, &status);
-    checkError(status, "Failed to create buffer for output");
   }
 #else
   char *source = 0;
@@ -182,91 +140,6 @@ bool init_opencl() {
   return true;
 }
 
-void init_problem(float *data) {
-    if (num_devices == 0) {
-        checkError(-1, "No devices");
-    }
-
-    input.reset(num_devices);
-    output.reset(num_devices);
-
-    float *ptr = data;
-
-    for (unsigned i = 0; i < num_devices; ++i) {
-        input[i].reset(n_per_device[i]);
-        output[i].reset(n_per_device[i]);
-
-        for (unsigned j = 0; j < n_per_device[i]; ++j) {
-            input[i][j] = *ptr;
-            ptr++;
-        }
-    }
-}
-
-void cl_run(int num_of_elements, float *data) {
-    cl_int status;
-
-    scoped_array<cl_event> kernel_event(num_devices);
-    scoped_array<cl_event> finish_event(num_devices);
-
-    // calculate the step for the bucket.
-    float max_num = *max_element(data, data+num_of_elements);
-    int step = ceil(max_num / num_of_elements);
-    int elem_in_bucket[NUM_BUCKETS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-    for (unsigned i = 0; i < num_devices; ++i) {
-        cl_event write_event[3];
-        status = clEnqueueWriteBuffer(queue[i], input_buf[i], CL_FALSE,
-                0, n_per_device[i] * sizeof(float), input[i], 0, NULL,
-                &write_event[0]);
-        checkError(status, "Failed to transfer input.");
-
-        status = clEnqueueWriteBuffer(queue[i], bucket_buf[i], CL_FALSE,
-                0, NUM_BUCKETS * sizeof(int), &elem_in_bucket, 0, NULL,
-                &write_event[1]);
-        checkError(status, "Failed to transfer elem_in_bucket.");
-
-        status = clEnqueueWriteBuffer(queue[i], step_buf[i], CL_FALSE,
-                0, 1 * sizeof(int), &step, 0, NULL,
-                &write_event[2]);
-        checkError(status, "Failed to transfer step.");
-
-        unsigned argi = 0;
-        status = clSetKernelArg(kernel[i], argi++, sizeof(cl_mem),
-                &input_buf[i]);
-        checkError(status, "Failed to set argument %d", argi - 1);
-
-        status = clSetKernelArg(kernel[i], argi++, sizeof(cl_mem),
-                &bucket_buf[i]);
-        checkError(status, "Failed to set argument %d", argi - 1);
-
-        status = clSetKernelArg(kernel[i], argi++, sizeof(cl_mem),
-                &step_buf[i]);
-        checkError(status, "Failed to set argument %d", argi - 1);
-
-        status = clSetKernelArg(kernel[i], argi++, sizeof(cl_mem),
-                &output_buf[i]);
-        checkError(status, "Failed to set argument %d", argi - 1);
-
-        const size_t global_work_size = n_per_device[i];
-        printf("Launching for device %d (%d elements)\n", i,
-                global_work_size);
-
-        status = clEnqueueNDRangeKernel(queue[i], kernel[i], 1, NULL,
-                &global_work_size, NULL, 2, write_event, &kernel_event[i]);
-        checkError(status, "Failed to launch kernel");
-
-        status = clEnqueueReadBuffer(queue[i], output_buf[i], CL_FALSE,
-                0, n_per_device[i] * sizeof(float), output[i], 1,
-                &kernel_event[i], &finish_event[i]);
-        clReleaseEvent(write_event[0]);
-        clReleaseEvent(write_event[1]);
-        clReleaseEvent(write_event[2]);
-
-    }
-    clWaitForEvents(num_devices, finish_event);
-}
-
 void cleanup() {
 #ifndef APPLE
   for(unsigned i = 0; i < num_devices; ++i) {
@@ -275,18 +148,6 @@ void cleanup() {
     }
     if(queue && queue[i]) {
       clReleaseCommandQueue(queue[i]);
-    }
-    if(input_buf && input_buf[i]) {
-        clReleaseMemObject(input_buf[i]);
-    }
-    if(bucket_buf && bucket_buf[i]) {
-        clReleaseMemObject(bucket_buf[i]);
-    }
-    if(step_buf && step_buf[i]) {
-        clReleaseMemObject(step_buf[i]);
-    }
-    if(output_buf && output_buf[i]) {
-        clReleaseMemObject(output_buf[i]);
     }
   }
 #else
